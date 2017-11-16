fd457afb4681879efd51eab1158998e4cbf0e682
Łukasz Dziedziul <l.dziedziul@gmail.com>
2017-11-16 23:11:11 +0100

    add static page and mvc test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,15 +2,27 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class DemoApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() throws Exception {
+        mockMvc.perform(get("/index.html"))
+                .andExpect(MockMvcResultMatchers.content()
+                        .string("Wprowadzenie do spring io"));
+    }
 
 }
