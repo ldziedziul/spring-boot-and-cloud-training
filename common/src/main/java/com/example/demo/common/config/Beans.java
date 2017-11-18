5ea5c53ed4aad405dad3e72c2df017b9a3d01880
Łukasz Dziedziul <l.dziedziul@gmail.com>
2017-11-18 19:24:50 +0100

    set prototype scope to mapper

--- common/src/main/java/com/example/demo/common/config/Beans.java
+++ common/src/main/java/com/example/demo/common/config/Beans.java
@@ -1,15 +1,22 @@
 package com.example.demo.common.config;
 
 import com.example.demo.common.Mapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class Beans {
+    private static final Logger log = LoggerFactory.getLogger(Beans.class);
 
     @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public Mapper mapper(MessageSource messageSource) {
+        log.error("Creating new mapper");
         return new Mapper(messageSource);
     }
 }
