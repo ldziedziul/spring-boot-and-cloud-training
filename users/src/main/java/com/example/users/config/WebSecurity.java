2ea505bb06112b552eedd38a3a670f111a2bcc63
Lukasz Dziedziul <lukasz.dziedziul@cronn.de>
2017-11-21 01:01:18 +0100

    add minimal setup for authorization server

---
+++ users/src/main/java/com/example/users/config/WebSecurity.java
@@ -0,0 +1,33 @@
+package com.example.users.config;
+
+import com.example.users.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@EnableAuthorizationServer
+@Configuration
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UsersService usersService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(usersService).passwordEncoder(passwordEncoder);
+    }
+
+}
