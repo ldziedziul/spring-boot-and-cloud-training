278ccda2a826d269eb952e2d9b116920351361d9
Lukasz Dziedziul <lukasz.dziedziul@cronn.de>
2017-11-22 02:50:20 +0100

    add gateway server

---
+++ gateway/src/main/java/com/example/demo/gateway/GatewayServer.java
@@ -0,0 +1,13 @@
+package com.example.demo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayServer {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServer.class);
+    }
+}
