e3bb46ebe22da02b38c2b8df6b74b3382c455daf
Lukasz Dziedziul <lukasz.dziedziul@cronn.de>
2017-11-22 02:50:42 +0100

    add sample zuul filter

--- gateway/src/main/java/com/example/demo/gateway/GatewayServer.java
+++ gateway/src/main/java/com/example/demo/gateway/GatewayServer.java
@@ -3,11 +3,16 @@ package com.example.demo.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
 public class GatewayServer {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServer.class);
+    }
+    @Bean
+    public SampleZuulFilter sampleZuulFilter() {
+        return new SampleZuulFilter();
     }
 }
